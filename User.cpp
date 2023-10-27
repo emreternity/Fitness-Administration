f43de44dee96b2476c8a24ecc148adeb56ef0aa7
Emre <115872768+emreternity@users.noreply.github.com>
2023-10-27 12:15:36 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <ctime>
 #include <chrono>
+#include <algorithm>
 
 User::User(string _fname, string _lname, int _age, float _weight, float _height, string _email, float _balance = 0, int _xp = 0) {
 	setFirstName(_fname);
@@ -44,6 +45,20 @@ string User::getLastName() const {
 	return lname;
 }
 
+void User::setUserType(string _usertype) {
+	_usertype = transform(_usertype.begin(), _usertype.end(), _usertype.begin(), ::tolower);
+	if (_usertype != "silver" || _usertype != "gold" || _usertype != "platinum" || _usertype != "diamond"){
+		throw invalid_argument("Uyelik tipi mevcut degil.")
+	}
+	else {
+		usertype = _usertype;
+	}
+}
+
+string User::getUserType() {
+	return usertype;
+}
+
 void User::setAge(int _age) {
 	if (_age < 14 || _age > 65) {
 		throw invalid_argument("GYM uyeligimiz icin yasiniz uygun degildir.");
@@ -83,16 +98,16 @@ float User::getHeight() const {
 	return height;
 }
 
-// TODO: Günlerin aya ve yýla göre sýnýrlandýrýlmasý saðlanacak, þubat ayýnýn 29 çektiði günler gibi özel durumlar göz önünde bulundurularak kodlanacak.
+// TODO: G?nlerin aya ve y?la g?re s?n?rland?r?lmas? sa?lanacak, ?ubat ay?n?n 29 ?ekti?i g?nler gibi ?zel durumlar g?z ?n?nde bulundurularak kodlanacak.
 // void User::setRegdate(int _regday, int _regmonth, int _regyear) { }
 
-// TODO: Bilgisayardan tarih verisini alan ve regday, regmonth, regyear verilerine ayrý ayrý dolduran setRegdate fonksiyonu kodlanacak.
+// TODO: Bilgisayardan tarih verisini alan ve regday, regmonth, regyear verilerine ayr? ayr? dolduran setRegdate fonksiyonu kodlanacak.
 // void User::setRegdate() { } 
 // string User::getRegdate() const { }
 
 void User::setEmail(string _email) {
 
-	// TODO: Girilen email deðerinin _________@____.com þeklindeki geleneksel email formatýna uyup uymadýðýný belirleyen doðrulama kodlanacak.
+	// TODO: Girilen email de?erinin _________@____.com ?eklindeki geleneksel email format?na uyup uymad???n? belirleyen do?rulama kodlanacak.
 
 	if (_email.length() > 256 || _email.length() < 1) {
 		throw invalid_argument("Gecersiz email uzunlugu.");
@@ -132,12 +147,12 @@ int User::getXP() const {
 	return xp;
 }
 
-// Switch Case kullanýlarak, verilen level deðerine karþýlýk gelen xp deðeri setXP fonk. ile ayarlanacak.
+// Switch Case kullan?larak, verilen level de?erine kar??l?k gelen xp de?eri setXP fonk. ile ayarlanacak.
 // void User::setLevel(int _level) { }
 
-// Switch Case kullanýlarak, sahip olunan XP deðerine karþýlýk gelen alt taban level deðeri return edilecek.
+// Switch Case kullan?larak, sahip olunan XP de?erine kar??l?k gelen alt taban level de?eri return edilecek.
 // int User::getLevel() const { }
 
 string User::getName() const {
-	return fname + " " + lname; // TODO: Deðer doðru mu kontrol edilecek.
+	return fname + " " + lname; // TODO: De?er do?ru mu kontrol edilecek.
 }
