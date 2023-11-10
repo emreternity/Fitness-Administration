cd98f4b7e6fc74cff45eb021d8e1492415ac3114
Emre <115872768+emreternity@users.noreply.github.com>
2023-11-10 11:37:06 +0300

    Initialization of class Usable

---
+++ Usable.h
@@ -0,0 +1,15 @@
+#ifndef USABLE_H
+#define USABLE_H
+
+#include <string>
+
+class Usable {
+	protected:
+		string name;
+		string accessLevel;
+		bool isReservable;
+	public:
+		
+};
+
+#endif // !USABLE_H
