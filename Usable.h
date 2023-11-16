a431e25e54a1ad68e3f6cee51f779b78a19948a3
Emre <115872768+emreternity@users.noreply.github.com>
2023-11-16 13:00:12 +0300

    Update Usable.h

--- Usable.h
+++ Usable.h
@@ -2,14 +2,39 @@
 #define USABLE_H
 
 #include <string>
+#include "Member.h"
 
 class Usable {
-	protected:
+	private:
+		int capacity;
 		string name;
+		string usableType;
 		string accessLevel;
 		bool isReservable;
-	public:
+		bool isReserved;
+		Member reserver;
 		
+	public:
+		explicit Usable(int _capacity, const string &_name, const string &_usableType = "equipment", const string &_accessLevel = "silver", bool _isReservable = true, Member _reserver = Member("N/A","N/A",20,90,50,"01/01/2000","silver","nullreserver@gmail.com",1));
+		~Usable();
+		void setCapacity(int _capacity);
+		void setName(const string &_name);
+		void setUsableType(const string &_usableType);
+		void setAccessLevel(const string &_accessLevel);
+		void setIsReservable(bool _isReservable);
+		void setReserver(Member _reserver);
+		
+		int getCapacity() const;
+		string getName() const;
+		string getUsableType() const;
+		string getAccessLevel() const;
+		bool getIsReservable() const;
+		bool getIsReserved() const;
+		Member getReserver() const;
+		
+		void reserve(Member _reserver);
+		void unreserve();
+		string getReserverInfo() const;
 };
 
 #endif // !USABLE_H
